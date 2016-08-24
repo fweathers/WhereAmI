805104c9755ace97c68d6a8ad5c824524d5ce856
Felicia Weathers <Fel.Weathers@gmail.com>
2016-08-24 06:31:18 -0400

    Print nearest address

--- ViewController.swift
+++ ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     var manager: CLLocationManager!
     
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -34,29 +34,83 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        print(locations)
-        
+        //print(locations)
         var userLocation: CLLocation = locations[0]
+        
         
         self.latitudeLabel.text = "\(userLocation.coordinate.latitude)"
         self.longitudeLabel.text = "\(userLocation.coordinate.longitude)"
-
+        
         self.courseLabel.text = "\(userLocation.course)"
         self.speedLabel.text = "\(userLocation.speed)"
         self.altitudeLabel.text = "\(userLocation.altitude)"
         
         
         
-        
-        
-        
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if (error != nil) {
+                
+                print(error)
+                
+            } else {
+                
+                if let placemark = placemarks?[0] {
+                    
+                    print(placemark)
+                    
+                    var subThoroughfare = ""
+                    
+                    if placemark.subThoroughfare != nil {
+                        
+                        subThoroughfare = placemark.subThoroughfare!
+                        
+                    }
+                    
+                    var thoroughfare = ""
+                    
+                    if placemark.thoroughfare != nil {
+                        
+                        thoroughfare = placemark.thoroughfare!
+                    }
+                    
+                    var sublocality = ""
+                    
+                    if placemark.subLocality != nil {
+                        
+                        sublocality = placemark.subLocality!
+                    }
+                    
+                    var subAdministrativeArea = ""
+                    
+                    if placemark.subAdministrativeArea != nil {
+                        
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                        
+                    }
+                    
+                    var postalCode = ""
+                    
+                    if placemark.postalCode != nil {
+                        
+                        postalCode = placemark.postalCode!
+                        
+                    }
+                    
+                    var country = ""
+                    
+                    if placemark.country != nil {
+                        
+                        country = placemark.country!
+                    }
+                    
+                    print(subThoroughfare + " " + thoroughfare + "\n" + sublocality + "\n" + subAdministrativeArea + "\n" + postalCode + "\n" + country)
+                }
+            }
+        }
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
